e41783b6cac9065f52c9016b29169ac6c7541dd5
pratik06102003 <pratik.yadav@joshtechnologygroup.com>
2025-09-01 11:10:19 +0530

    YP_RU_00: Base Setup: Configured Autoprefixer

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -37,7 +37,7 @@ export default {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
 
       {
